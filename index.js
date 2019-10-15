e9a961de5ca3552a756c4c93465164f656599f94
Bernard D. White <danquahwhite@gmail.com>
2019-10-15 04:35:40 +0000

    deleted tar file

--- index.js
+++ index.js
@@ -1,51 +1,71 @@
 const path = require('path')
-const express =  require('express')
-const app =  express()
-const port = process.env.PORT || 3000
+const express = require('express')
 const MongoClient = require('mongodb').MongoClient
-const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/dev'
-const multer = require('multer');
+const multer = require('multer')
+const marked = require('marked')
 
-async function initMongo(){
-    console.log('Initialising MongoDB...');
-    let success = false;
-    while(!success){
-        try {
-            client = await MongoClient.connect(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true})
-            success = true
-        } catch{
-            console.log('Error connecting to MongoDB, retrying in 1 second')
-            await new Promise(resolve => setTimeout(resolve, 1000))
-        }
+const app = express()
+const port = process.env.PORT || 3000
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/knots'
+
+async function initMongo() {
+  console.log('Initialising MongoDB...')
+  let success = false
+  while (!success) {
+    try {
+      client = await MongoClient.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true})
+      success = true
+    } catch {
+      console.log('Error connecting to MongoDB, retrying in 1 second')
+      await new Promise(resolve => setTimeout(resolve, 1000))
     }
-    console.log('MongoDB initialized')
-    return client.db(client.s.options.dbName).collection('notes');
+  }
+  console.log('MongoDB initialised')
+  return client.db(client.s.options.dbName).collection('notes')
 }
 
+async function start() {
+  const db = await initMongo()
 
+  app.set('view engine', 'pug')
+  app.set('views', path.join(__dirname, 'views'))
+  app.use(express.static(path.join(__dirname, 'public')))
 
-async function start(){
-    const db =  await initMongo();
-    app.set('view engine', 'pug');
-    app.set('views', path.join(__dirname, 'views'))
-    app.use(express.static(path.join(__dirname, 'public')))
-    app.listen(port, ()=> {
-        console.log(`App listening on http://localhost:${port}`)
-    })
+  app.get('/', async (req, res) => {
+    res.render('index', { title:'Knote App', notes: await retrieveNotes(db) })
+  })
 
-    app.get('/', async (req, res) => {
-        res.render('index', {notes:  retrieveNotes(db)})
-    })
+  app.post(
+    '/note',
+    multer({ dest: path.join(__dirname, 'public/uploads/') }).single('image'),
+    async (req, res) => {
+      if (!req.body.upload && req.body.description) {
+        await saveNote(db, { description: req.body.description })
+        res.redirect('/')
+      } else if (req.body.upload && req.file) {
+        const link = `/uploads/${encodeURIComponent(req.file.filename)}`
+        res.render('index', {
+          content: `${req.body.description} ![](${link})`,
+          notes: await retrieveNotes(db),
+        })
+      }
+    },
+  )
 
-    // app.post('/note', multer().none(), async)
+  app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`)
+  })
 }
 
-async function retrieveNotes(db){
-    const notes  = (await db.find().toArray()).reverse()
-    return notes
+async function saveNote(db, note) {
+  await db.insertOne(note)
 }
 
-async function saveNote(db, note){
-    await db.insertOne(note)
+async function retrieveNotes(db) {
+  const notes = (await db.find().toArray()).reverse()
+  return notes.map(it => {
+    return { ...it, description: marked(it.description) }
+  })
 }
+
 start()
